b56649b114e2778bb3d395cb72b1f798e873619a
Steve Whittaker <swhitt@gmail.com>
2019-04-03 17:40:32 -0500

    Find gpg-connect-agent socket easier

--- home/.functions.sh
+++ home/.functions.sh
@@ -82,14 +82,14 @@ function _update_agents {
     # take over SSH keychain (with gpg-agent soon) but only on local machine, not remote ssh machine
     # keychain used in a non-invasive way where it's up to you to add your keys to the agent.
     if [ ! "$SSH_CONNECTION" ] && which gpg-connect-agent &>/dev/null; then
-		export SSH_AUTH_SOCK=$(gpgconf --list-dirs | grep agent-ssh-socket | cut -f 2 -d :)
+		export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
         # start GPG agent, and update TTY. For the former only, omit updatestartuptty
         # ssh-agent protocol can't tell gpg-agent/pinentry what tty to use, so tell it
         # if GPG agent has locked up or there is a stale remote agent, remove
         # the stale socket and possible local agent.
         if ! timeout -k 2 1 gpg-connect-agent updatestartuptty /bye > /dev/null; then
             echo "Removing stale GPG agent"
-            socket=$(gpgconf --list-dirs | grep agent-socket | cut -f 2 -d :)
+            socket=$(gpgconf --list-dirs agent-ssh-socket)
             test -S $socket && rm $socket
             killall -KILL gpg-agent 2> /dev/null
             # try again
