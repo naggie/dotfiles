519c5a84c220d79b6501ff12c12a4f8920d39c77
Callan Bryant <callan.bryant@gmail.com>
2021-02-10 03:55:37 +0000

    add bluetooth fixing script

---
+++ etc/fix-bluetooth.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+sudo killall coreaudiod
+sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
+sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
