670793bda0b3751fe157196edb2850aebd9e8a40
Callan Bryant <callan.bryant@gmail.com>
2021-03-24 23:06:43 +0000

    handle changing names

--- etc/headset.sh
+++ etc/headset.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -ex
 # https://askubuntu.com/questions/14077/how-can-i-change-the-default-audio-device-from-command-line
 
 # See also https://github.com/mk-fg/python-pulse-control for later streamdeck integration
@@ -9,12 +8,17 @@ set -ex
 # reload:
 #pulseaudio -k
 
-# list -- can change between hosts (could use regex matcher, but I think it's fine to update this whenever)
+# list -- can change between hosts (could use regex matcher, but I think it's fine to update this whenever **THIS IS NECESSARY -- device name changes on replug**)
 #pactl list short sinks
 #pactl list short sources
+# note active device is RUNNING, else SUSPENDED or IDLE
 
 pacmd set-default-sink "alsa_output.usb-Logitech_G533_Gaming_Headset-00.iec958-stereo"
+pacmd set-default-sink "alsa_output.usb-Logitech_G533_Gaming_Headset-00.analog-stereo"
+
+# name changes so try both
 pacmd set-default-source "alsa_output.usb-Logitech_G533_Gaming_Headset-00.iec958-stereo.monitor"
+pacmd set-default-source "alsa_output.usb-Logitech_G533_Gaming_Headset-00.analog-stereo.monitor"
 
 # volume control (65536 = 100 %, 0 = mute; or a bit more intuitive 0x10000 =
 # 100 %, 0x7500 = 75 %, 0x0 = 0 %)
--- etc/speakers.sh
+++ etc/speakers.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-# https://askubuntu.com/questions/14077/how-can-i-change-the-default-audio-device-from-command-line
-
-# in /etc/pulse/default.pa
-#load-module module-stream-restore restore_device=false
-# reload:
-#pulseaudio -k
-
-# list
-#pactl list short sinks
-#pactl list short sources
+# see headset.sh for commentary
 
 pacmd set-default-sink "alsa_output.pci-0000_0a_00.4.analog-stereo"
-
-# volume control (65536 = 100 %, 0 = mute; or a bit more intuitive 0x10000 =
-# 100 %, 0x7500 = 75 %, 0x0 = 0 %)
 pacmd set-sink-volume "alsa_output.pci-0000_0a_00.4.analog-stereo" 0x9000
