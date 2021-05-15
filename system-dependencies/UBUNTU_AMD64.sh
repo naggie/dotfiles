0f2c6b41fe1dec4eb4d7fae916b644348012129f
Callan Bryant <callan.bryant@gmail.com>
2021-05-15 11:00:15 +0100

    do not install adhoc hugo

--- system-dependencies/UBUNTU_AMD64.sh
+++ system-dependencies/UBUNTU_AMD64.sh
@@ -77,7 +77,7 @@ adhoc_ripgrep_linux_amd64
 adhoc_fzf_linux_amd64
 adhoc_neovim_linux_amd64
 adhoc_golang_linux_amd64
-adhoc_hugo_linux_amd64
+#adhoc_hugo_linux_amd64
 adhoc_ffsend_linux_amd64
 
 # GUI if applicable
